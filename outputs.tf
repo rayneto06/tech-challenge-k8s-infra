d91f5f183ee9a90f299e454ac28ba0ece32f5cb8
Raymundo Neto <raymundocneto@gmail.com>
2025-05-28 13:19:27 -0300

    fix(eks): use correct node_groups/worker_groups argument

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,7 @@
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
-
 output "cluster_endpoint" {
-  description = "API server endpoint"
-  value       = module.eks.cluster_endpoint
+  value = module.eks.cluster_endpoint
 }
 
-output "cluster_ca_certificate" {
-  description = "Base64 encoded CA cert"
-  value       = module.eks.cluster_certificate_authority_data
-}
-
-output "kubeconfig" {
-  description = "Kubeconfig file contents"
-  value       = module.eks.kubeconfig
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data[0]
 }
